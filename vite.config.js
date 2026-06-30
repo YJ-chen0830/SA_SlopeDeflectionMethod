7f5fa70a4c6f4cfecd4732237ba1efffae2d68ce
YJ-chen0830 <143475355+YJ-chen0830@users.noreply.github.com>
2026-06-30 09:12:12 +0800

    change the trigger from github to control center

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/SA_SlopeDeflectionMethod/',   // ← 加這行
   plugins: [react()],
 })
